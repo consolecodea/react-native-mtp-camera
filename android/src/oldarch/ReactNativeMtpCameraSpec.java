6ecacd7a392f9f60dbbe715e2a8708e6f076e7ee
abhinandvk41 <abhinandvk41@gmail.com>
2024-07-09 18:15:05 +0530

    feat(android): bug fix

--- android/src/oldarch/ReactNativeMtpCameraSpec.java
+++ android/src/oldarch/ReactNativeMtpCameraSpec.java
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.Promise;
 
 abstract class ReactNativeMtpCameraSpec extends ReactContextBaseJavaModule {
-  NativeMtpCameraSpec(ReactApplicationContext context) {
+  ReactNativeMtpCameraSpec(ReactApplicationContext context) {
     super(context);
   }
 
